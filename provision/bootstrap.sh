8189502a02e30773305fb7255b20afa76f38fb0b
rdrgporto <rdrgporto@gmail.com>
2021-02-14 15:44:12 +0100

    Update to Docker 20.10.3 & Docker Compose 1.28.2 :sparkles:

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -8,8 +8,8 @@ DATE() {
 # Variables
 IP=`ip -o addr show up primary scope global | while read -r num dev fam addr rest; do echo [$(DATE)] [Info] [System] ${addr%/*}; done`
 VM_USER=vagrant
-DOCKER_VERSION=20.10.0
-DOCKER_COMPOSE_VERSION=1.27.4
+DOCKER_VERSION=20.10.3
+DOCKER_COMPOSE_VERSION=1.28.2
 
 # Non-Interactive Installation
 export DEBIAN_FRONTEND=noninteractive
