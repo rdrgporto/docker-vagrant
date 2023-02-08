e16838082cd5becf1ab9a59639ac3c273009c7c0
rdrgporto <rdrgporto@gmail.com>
2023-02-08 21:06:17 +0100

    Update image Docker of Golang and Alpine in Golang App :package:

--- dockerfiles/golang/code/main.go
+++ dockerfiles/golang/code/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+  "fmt"
+  "net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
-    })
-
-    http.ListenAndServe(":9000", nil)
+  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to my website!")
+  })
+  http.ListenAndServe(":9000", nil)
 }
