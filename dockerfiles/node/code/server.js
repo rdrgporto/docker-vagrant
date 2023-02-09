f29309279c4bb53244d0d0c34b4682fe13d00c15
rdrgporto <rdrgporto@gmail.com>
2023-02-09 21:01:55 +0100

    Update image Docker of NodeJS in Node App :package:

--- dockerfiles/node/code/server.js
+++ dockerfiles/node/code/server.js
@@ -1,11 +1,11 @@
 var express = require('express'),
-    path = require('path'),
-    app = express();
+  path = require('path'),
+  app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.redirect('index.html');
+  res.redirect('index.html');
 });
 
 app.listen(9000);
